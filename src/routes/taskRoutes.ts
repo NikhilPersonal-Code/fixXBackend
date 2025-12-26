827e37bed7f86bdc736f282b55abdf2006760549
NikhilRW <nikhilwankhede0707@gmail.com>
2025-12-26 19:32:53 +0530

    chore:testing push to both repositories

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -12,7 +12,6 @@ import verifyToken from '@middleware/auth';
 
 const router = Router();
 
-
 // Public routes
 router.get('/', getTasks); // Get all posted tasks (for feed)
 router.get('/latest', getLatestTasks); // Get latest tasks (for notifications)
