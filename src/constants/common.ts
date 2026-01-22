617859c876635a72f36cb18f0897054ec80f2ba6
NikhilRW <nikhilwankhede0707@gmail.com>
2026-01-22 13:06:29 +0530

    refactor:Refactored Pool Config To Use DB Url
    - Continuation - Instead Of The separately passing all options like username,password,host,database name,port,scheme

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,11 +1,7 @@
 import { PoolConfig } from 'pg';
 
 export const dbConfig: PoolConfig = {
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
   // Connection pool settings
   max: 10, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
